dfc4b2a9e77ca8673f9b0cce59427c846fb162f4
shreyas7057 <shreyas7057@gmail.com>
2021-12-25 19:45:40 +0530

    name change

--- 50.py
+++ 50.py
@@ -1,8 +1,8 @@
-str1 = "Welcome to USA. usa awesome, isn't it?"
+str1 = "Welcome to America. america awesome, isn't it?"
 
-findStr = "USA"
+findStr = "America"
 
 newStr = str1.upper()
 
 countNum = newStr.count(findStr)
-print(f"The number of times USA come is {countNum}")
+print(f"The number of times America come is {countNum}")
